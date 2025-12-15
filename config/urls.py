befb31921b78d0f70f082c167372fe17349fe9bb
Tate Jones <tnj@njit.edu>
2025-12-15 07:04:55 -0500

    Wrap LoginView with csrf_exempt to fix CSRF errors

--- config/urls.py
+++ config/urls.py
@@ -20,11 +20,12 @@ from django.contrib.auth import views as auth_views
 from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('assignments/', include('assignments.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', csrf_exempt(auth_views.LoginView.as_view(template_name='registration/login.html')), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('', lambda request: redirect('dashboard')),
 ]
